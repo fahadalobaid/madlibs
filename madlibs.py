b383f73aec73417186fc036bd5c55d82efa05fc6
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-03 19:52:59 +0300

    first py project

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,17 @@
 def main():
 	# write your code here
+	time = input("Enter a number from 1 to 12")
+	item = input("Enter a noun (plural)")
+	name = input("Enter a name")
+
+	scream = input("Enter any sentence")
+
+	action = input("Enter a verb")
+
+	print(f" It was {time} when I heard a knock at the door")
+	print(f"I opened the door and there was a box full of {item} with a note saying From Mr. {name.capitalize()}.")
+	print(f"Just as I closed the door I heard a scream{scream.upper()}")
+	print(f"I froze in place and all I could do was{action}")
 
 
 
